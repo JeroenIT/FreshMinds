7fdf283da3939e0276fa9c1e1864dd8b8010fc0d
Kareem Mohamed <kareem.mohamed@student.fontys.nl>
2025-06-18 11:24:00 +0200

    6/18/25

--- Backend/FreshMindsApi/Program.cs
+++ Backend/FreshMindsApi/Program.cs
@@ -1,40 +1,41 @@
 var builder = WebApplication.CreateBuilder(args);
-
-
-
 var app = builder.Build();
 
-
-
-app.MapGet("/", () => "Welkom op de API");
-app.MapGet("/users", () => new List<string> { "Alice", "Bob", "Charlie" });
-app.MapGet("/activities", () => new List<Activity>()
+var activities = new List<Activity>()
 {
     new Activity("Barbeque", "Eindhoven", 14.99, 5, "12-7-2025"),
     new Activity("Spelletjesavond", "Eindhoven", 9.99, 3, "15-7-2025"),
     new Activity("Filmavond", "Eindhoven", 19.99, 4, "20-7-2025"),
-    new Activity("Concert", "Utrecht", 13.99, 50, "5-8-2025"),
+    new Activity("Concert", "Utrecht", 13.99, 50, "5-8-2025")
+};
+var duties = new List<Duty>()
+{
+    //new Duty("naam", "beschrijving", aantal mensen, prioriteit, taakgrootte, "datum"),
+    new Duty("lamp vervangen", "keuken", 2, "ja","geen expert", "11-6-2025"),
+    new Duty("badkamer schoonmaken", "hoofdlamp", 2, "nee","geen expert", "12-6-2025"),
+    new Duty("kapotte koelkast maken", "keuken", 3, "ja","expert", "30-6-2025")
+};
+app.MapPost("/activities", async (Activity newActivity) =>
+{
+    System.Console.WriteLine($"Nieuwe activiteit ontvangen: {newActivity.Name}");
+    activities.Add(newActivity);
+    await Task.Delay(5);
+    return Results.Created($"/activities/{newActivity.Name}", newActivity);
 });
 
-app.MapGet("/objects", () => new List<User>()
+app.MapPost("/duties", async (Duty newDuty) =>
 {
-    new User("Alice", 30),
-    new User("Bob", 25),
-    new User("Charlie", 35)
+    System.Console.WriteLine($"Nieuwe taken ontvangen: {newDuty.Name}");
+    duties.Add(newDuty);
+    await Task.Delay(5);
+    return Results.Created($"/duties/{newDuty.Name}", newDuty); 
 });
+
+app.MapGet("/", () => "Welkom op de API");
+app.MapGet("/activities", () => activities);
+app.MapGet("/duties", () => duties);
+
 app.Run("http://0.0.0.0:5000");
-
-public class User
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-
-    public User(string name, int age)
-    {
-        Name = name;
-        Age = age;
-    }
-}
 
 public class Activity
 {
@@ -50,6 +51,26 @@ public class Activity
         Place = place;
         Amount = amount;
         Price = price;
+        Date = date;
+    }
+}
+
+public class Duty
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int AmountOfPeople { get; set; }
+    public string Priority { get; set; }
+    public string TaskSize { get; set; }
+    public string Date { get; set; }
+
+    public Duty(string name, string description, int amountOfPeople, string priority, string taskSize, string date)
+    {
+        Name = name;
+        Description = description;
+        AmountOfPeople = amountOfPeople;
+        Priority = priority;
+        TaskSize = taskSize;
         Date = date;
     }
 }
